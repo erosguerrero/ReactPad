ddc3a015cce466654f91eb3fba1b8a852c1ac1b0
PabloMartinez01 <pablomartinezcoder@gmail.com>
2023-04-28 13:16:25 +0200

    Buscar libros

--- google-books-client/src/App.js
+++ google-books-client/src/App.js
@@ -14,26 +14,40 @@ function Form() {
 
     const [inputText, setInputText] = useState("");
     const [displayText, setDisplayText] = useState(false);
+
     const inputTextHandler = (e) => {
-    const value = e.target.value;
-    setDisplayText(false);
-    setInputText(value);
+      const value = e.target.value;
+      setDisplayText(false);
+      setInputText(value.trim());
     };
     const onSubmit = (e) => {
-    e.preventDefault();
-    setDisplayText(true);
+      e.preventDefault();
+      if (inputText)
+        setDisplayText(true);
     };
 
     return (
       <>
       
-      <form onSubmit={onSubmit}>
-      <input type="text"
-      onChange={inputTextHandler}></input>
-      <button type="submit">Aceptar</button>
-      </form>
+      <form className='container w-50 mt-4' onSubmit={onSubmit}>
+      <div className='row form-group'>
+        <label className='form-label text-start'>Título o autor:</label>
+        <input 
+          className='form-control '
+          type="text"
+          name='titulo'
+          id='titulo'
+          placeholder='Busca un título o autor'
+          onChange={inputTextHandler}>
+        </input>
+      </div>
+      <div className='text-end mt-3'>
+        <button className='btn btn-primary' type="submit">Aceptar</button>
+      </div>
+      
+     </form>
 
-      {displayText ? <h1>Buscando: {inputText}</h1> : null}
+      {displayText ? <h1 className='mt-3'>Buscando: {inputText}</h1> : null}
         
       </>
       );
